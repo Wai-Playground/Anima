eaef5eb449a53d7e4e414a824205754253b23833
Wai <Mincl5792@gmail.com>
2021-11-21 19:41:46 -0500

    removed unesassary imports

--- tomoEngine/novel.ts
+++ tomoEngine/novel.ts
@@ -6,27 +6,26 @@
 import {
   ButtonInteraction,
   CommandInteraction,
-  Message,
   MessageAttachment,
-  MessageComponent,
   MessageActionRow,
   SelectMenuInteraction,
   MessageButton,
-  MessagePayload,
-  ButtonInteractionCollectorOptions,
-  SelectMenuInteractionCollectorOptions,
   InteractionCollector,
   MessageSelectMenu,
   MessageSelectOptionData,
 } from "discord.js";
 import engineBase from "./base";
-import { NovelError, TomoError } from "./statics/errors";
-import { single, moodType, scripts,  } from "./statics/types";
+import { single, scripts } from "./statics/types";
 import Background from "./tomoClasses/backgrounds";
 import Character from "./tomoClasses/characters";
 
-import { Canvas, createCanvas, Image, loadImage, NodeCanvasRenderingContext2D} from "canvas";
-import { APIMessage } from "discord-api-types";
+import {
+  Canvas,
+  createCanvas,
+  Image,
+  loadImage,
+  NodeCanvasRenderingContext2D,
+} from "canvas";
 
 class NodeSingle {
   index: number;
@@ -38,10 +37,9 @@ class NodeSingle {
   route?: number | string;
   built: boolean = false;
   built_img: MessageAttachment;
-  choices?: Array<MessageSelectOptionData>
+  choices?: Array<MessageSelectOptionData>;
   placeholder?: string;
-  lookUpArr?: Array<scripts | number> 
-
+  lookUpArr?: Array<scripts | number>;
 
   constructor(single: single, index: number = null) {
     this.index = single.index || index;
@@ -50,24 +48,24 @@ class NodeSingle {
     this.text = single.text;
     this.mood = single.hasOwnProperty("mood") ? single.mood : null;
     this.route = single.hasOwnProperty("route") ? single.route : index + 1;
-  
+
     this.isChoiced = single.hasOwnProperty("args") ? true : false;
     if (this.isChoiced) {
-      this.choices = []
-      this.lookUpArr = []
+      this.choices = [];
+      this.lookUpArr = [];
       let i = 0;
-      this.placeholder = single.hasOwnProperty("placeholder") ? single.placeholder : "Select an option..."
+      this.placeholder = single.hasOwnProperty("placeholder")
+        ? single.placeholder
+        : "Select an option...";
       for (const arg of single.args) {
         this.choices.push({
           label: arg.label,
-          value: i.toString()
-        })
-        this.lookUpArr.push(arg.route)
-        console.log(this.choices)
+          value: i.toString(),
+        });
+        this.lookUpArr.push(arg.route);
+        console.log(this.choices);
         i++;
       }
-
-      
     }
   }
 }
@@ -80,7 +78,7 @@ export default class Novel extends engineBase {
   multiples: Array<single>;
   nodes: Array<NodeSingle>;
   index: number;
-  height: number = 1080;
+  height: number = 480;
   width: number = 720;
   selection: number;
   ephemeral: boolean;
@@ -102,7 +100,7 @@ export default class Novel extends engineBase {
     json: object,
     interaction: CommandInteraction | ButtonInteraction | SelectMenuInteraction,
     ephemeral: boolean
-  ) { 
+  ) {
     super(interaction.user, interaction);
 
     // Declare variables.
@@ -117,29 +115,33 @@ export default class Novel extends engineBase {
     this.prepareAssets();
   }
 
-
   async buildNode(index: number = this.index): Promise<MessageAttachment> {
-    const canvas: Canvas = createCanvas(1080, 720);
+    const canvas: Canvas = createCanvas(this.width, this.height);
     const ctx: NodeCanvasRenderingContext2D = canvas.getContext("2d");
 
-    const bg: Image = await loadImage(this.backgrounds.get(this.nodes[index].background).link);
-    const ch: Image = await loadImage(this.characters.get(this.nodes[index].character).link);
+    const bg: Image = await loadImage(
+      this.backgrounds.get(this.nodes[index].background).link
+    );
+    const ch: Image = await loadImage(
+      this.characters.get(this.nodes[index].character).link
+    );
 
-    ctx.drawImage(bg, 0, 0, bg.naturalWidth, bg.naturalHeight);
+    ctx.drawImage(bg, 0, 0, this.width, this.height);
     ctx.drawImage(ch, 0, 0, ch.naturalWidth, ch.naturalHeight);
 
     this.nodes[index].built = true;
-    this.nodes[index].built_img = new MessageAttachment(canvas.toBuffer(), `novel_userID_${this.interaction.user.id}_node_${index}_${this.name}.png`);
+    this.nodes[index].built_img = new MessageAttachment(
+      canvas.toBuffer("image/jpeg"),
+      `novel_userID_${this.interaction.user.id}_node_${index}_${this.name}.jpg`
+    );
 
     return this.nodes[index].built_img;
-
   }
 
   parseScript(str: scripts): void {
     console.log(str);
 
-
-    switch(str) {
+    switch (str) {
       case "$end":
         this.end();
         break;
@@ -149,9 +151,8 @@ export default class Novel extends engineBase {
         this.setPage(this.index + 1);
         break;
       case "$flag_g":
-        break;      
+        break;
     }
-
   }
 
   /**
@@ -173,39 +174,31 @@ export default class Novel extends engineBase {
      * Purpose | Loop through multiples and grab and store unique id of backgrounds and characters.
      */
     for (const single of this.multiples) {
-
-      
-      if (single.bg == undefined) {
-        console.error("[e] Does not have bg! grabbing one from behind us which is: " + this.nodes[i - 1].background)
-        
-        single.bg = this.nodes[i - 1].background; // If there is no bg in this single then we take one from behind us.
-      }
-      if (single.character == undefined) {
-        console.error("[e] Does not have ch! grabbing one from behind us which is: " + this.nodes[i - 1].character)
+      if (single.bg == undefined) single.bg = this.nodes[i - 1].background; // If there is no bg in this single then we take one from behind us.
+      if (single.character == undefined)
         single.character = this.nodes[i - 1].character; // If there is no char in this single then we take one from behind us.
 
-      }
-
       if (!this.backgrounds.has(single.bg)) {
-        
         // If the background map has this specific background ID already we skip. If not...
         payload = await this.getBackground(single.bg); // we store the payload of the db request
-        
+
         this.backgrounds.set(single.bg, payload); // we set it in this map
       }
 
-      //console.log(i + `_${single.mood}`);
+      // console.log(i + `_${single.mood}`);
       if (!this.characters.has(single.character) || single.mood != undefined) {
-        if (!this.characters.has(single.character)) console.log("[ch] Char not logged, getting the payload. Payload ID:" + single.character);
-        
-        
+        if (!this.characters.has(single.character))
+          console.log(
+            "[ch] Char not logged, getting the payload. Payload ID:" +
+              single.character
+          );
+
         // If the character mpa has the char id already we skip.
         payload = await this.getCharacter(single.character);
-        
+
         if (single.mood) {
-          
           payload = await payload.getVariant(single.mood); // If the character is a variant, we can substitute
-         
+
           single.character = payload.getId();
         }
 
@@ -217,23 +210,19 @@ export default class Novel extends engineBase {
       i++;
     }
 
-
     // Build the node length if the legnth is less than or equal to 10.
-    
+
     if (this.nodes.length <= 10) {
-      for (const node of this.nodes) { // for every node in here
+      for (const node of this.nodes) {
+        // for every node in here
         this.buildNode(node.index); // call the build function
       }
     }
 
-
-    
     process.nextTick(() => {
-        this.emit("ready");
-      });
-    
+      this.emit("ready");
+    });
   }
-
 
   /**
    * Name | start();
@@ -243,14 +232,18 @@ export default class Novel extends engineBase {
 
   async start() {
     const payload = {
-      content: `>>> ${this.characters.get(this.nodes[0].character).name} >> ${this.nodes[0].text}`,
-      files: [(this.nodes[0].built ? this.nodes[0].built_img : await this.buildNode(0))],
+      content: `>>> ${this.characters.get(this.nodes[0].character).name} >> ${
+        this.nodes[0].text
+      }`,
+      files: [
+        this.nodes[0].built ? this.nodes[0].built_img : await this.buildNode(0),
+      ],
       //attachments: [build],
-      components: await this.action()
-    }
+      components: await this.action(),
+    };
     if (this.interaction.isCommand()) {
       if (this.interaction.deferred) {
-        console.log("deferred")
+        console.log("deferred");
         await this.interaction.editReply(payload);
       } else {
         await this.interaction.reply(payload);
@@ -259,7 +252,7 @@ export default class Novel extends engineBase {
     if (this.interaction.isButton()) {
       if ("update" in this.interaction) await this.interaction.update(payload);
     }
-  
+
     this.message = await this.interaction.fetchReply();
 
     this.collectButton(this.filter);
@@ -270,16 +263,21 @@ export default class Novel extends engineBase {
     if (index < 0 || index > this.nodes.length - 1) return;
     this.index = index;
     const payload = {
-      content: `>>> ${this.characters.get(this.nodes[index].character).name} >> ${this.nodes[index].text}`,
-      files: [(this.nodes[index].built ? this.nodes[index].built_img : await this.buildNode(index))],
+      content: `>>> ${
+        this.characters.get(this.nodes[index].character).name
+      } >> ${this.nodes[index].text}`,
+      files: [
+        this.nodes[index].built
+          ? this.nodes[index].built_img
+          : await this.buildNode(index),
+      ],
       attachments: [],
-      components: await this.action()
-    }
-    await this.interaction.editReply(payload)
+      components: await this.action(),
+    };
+    await this.interaction.editReply(payload);
     if (this.nodes[this.index].isChoiced) this.collectSelect(this.filter);
-    
-    //await this.interaction.editReply(payload);
 
+    //await this.interaction.editReply(payload);
   }
 
   private async collectButton(filter: Function) {
@@ -292,57 +290,64 @@ export default class Novel extends engineBase {
       const button = interaction.customId.match(/(\d{1,1})/g)[0];
       let skript: scripts | number;
       skript = this.nodes[this.index].route as scripts | number;
-      if (typeof skript == 'string') return this.parseScript(skript);
+      if (typeof skript == "string") return this.parseScript(skript);
 
-      this.emit("buttonCollected", button)
-      this.buttonCollector.on('end', () => {
+      this.emit("buttonCollected", button);
+      this.buttonCollector.on("end", () => {
         this.emit("end");
-      
       });
 
       switch (parseInt(button)) {
         case 0:
-          this.setPage(this.index - 1)
+          this.setPage(this.index - 1);
           break;
         case 1:
-          
-          
           this.setPage(this.index + 1);
 
           break;
         case 2:
           if (this.selection != undefined) {
-            console.log("confirmed")
-            skript = this.nodes[this.index].lookUpArr[this.selection] as scripts & number;
+            console.log("confirmed");
+            skript = this.nodes[this.index].lookUpArr[
+              this.selection
+            ] as scripts & number;
             if (isNaN(skript)) return this.parseScript(skript);
             this.setPage(skript);
           }
-          
+
           break;
-
-
       }
     });
   }
 
+  /**
+   *
+   * @returns Message Action Row
+   */
   async action(): Promise<MessageActionRow[]> {
     let ret: Array<MessageActionRow> = [];
     const buttons = [
       {
-        disabled: (this.index > 0 ? false : true) || (this.nodes[this.index - 1].isChoiced),
+        disabled:
+          (this.index > 0 ? false : true) ||
+          this.nodes[this.index - 1].isChoiced,
         label: "Back",
         style: 1,
       },
       {
-        disabled: (this.index >= this.nodes.length - 1? true : false) || (this.nodes[this.index].isChoiced),
+        disabled:
+          (this.index >= this.nodes.length - 1 ? true : false) ||
+          this.nodes[this.index].isChoiced,
         label: "Next",
         style: 1,
       },
       {
-        disabled: !(this.nodes[this.index].isChoiced && (this.selection != undefined)),
+        disabled: !(
+          this.nodes[this.index].isChoiced && this.selection != undefined
+        ),
         label: "Confirm Selection",
         style: 1,
-      }
+      },
     ];
 
     let i = 0;
@@ -354,31 +359,38 @@ export default class Novel extends engineBase {
           .setDisabled(
             button.hasOwnProperty("disabled") ? button.disabled : false
           )
-          .setCustomId("NOVEL.button_" + i.toString() + "_user_" + this.interaction.user.id)
+          .setCustomId(
+            "NOVEL.button_" + i.toString() + "_user_" + this.interaction.user.id
+          )
           .setLabel(button.hasOwnProperty("label") ? button.label : null)
           //.setEmoji(button.hasOwnProperty("emj") ? button.emj : null)
           .setStyle(button.style)
       );
       i++;
     }
-    ret.push(buttonRow)
+    ret.push(buttonRow);
     if (this.nodes[this.index].isChoiced) {
       const selectRow = new MessageActionRow().addComponents(
         new MessageSelectMenu()
           .setCustomId(
-            "NOVEL.select_" + this.index.toString() + "_user_" + this.interaction.user.id
+            "NOVEL.select_" +
+              this.index.toString() +
+              "_user_" +
+              this.interaction.user.id
           )
-          .setPlaceholder(this.selection == undefined ? this.nodes[this.index].placeholder : this.nodes[this.index].choices[this.selection].label)
+          .setPlaceholder(
+            this.selection == undefined
+              ? this.nodes[this.index].placeholder
+              : this.nodes[this.index].choices[this.selection].label
+          )
           .addOptions(this.nodes[this.index].choices)
       );
-    
-      ret.push(selectRow)
-      
+
+      ret.push(selectRow);
     }
     //console.log(buttonRow)
 
     return ret;
-
   }
 
   private async collectSelect(filter: Function) {
@@ -387,34 +399,31 @@ export default class Novel extends engineBase {
       componentType: "SELECT_MENU",
       time: 60000,
     });
-    this.selectCollector.on("collect", async (interaction: SelectMenuInteraction) => {
-      let value = interaction.values[0] // value[1] = selection, value[2] = index
-      console.log(value)
+    this.selectCollector.on(
+      "collect",
+      async (interaction: SelectMenuInteraction) => {
+        let value = interaction.values[0]; // value[1] = selection, value[2] = index
+        console.log(value);
 
-      if (this.index == parseInt(value)) return;
-      this.selection = parseInt(value)
+        if (this.index == parseInt(value)) return;
+        this.selection = parseInt(value);
 
-      this.emit("selectCollected", value)
+        this.emit("selectCollected", value);
 
-      this.interaction.editReply({components: await this.action()})
-      
-    });
+        this.interaction.editReply({ components: await this.action() });
+      }
+    );
 
-    this.selectCollector.on('end', () => {
+    this.selectCollector.on("end", () => {
       this.emit("end");
-      
     });
   }
 
   public async end() {
-    this.emit("end")
+    this.emit("end");
     if (/*!this.selectCollector.ended ||*/ !this.buttonCollector.ended) {
       /*(await this.selectCollector.stop()*/
-      this.buttonCollector.stop()
-    
+      this.buttonCollector.stop();
     }
- 
-   
-    
   }
 }
